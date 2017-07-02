b3133ae0b2b9fd42f28d9b35c0b348b33ed18009
Your Name <you@example.com>
2017-07-02 23:02:16 +0800

    v6.0

--- item.md
+++ item.md
@@ -144,5 +144,154 @@ http://www.adidas.com.cn/specific/product/ajaxview/?id=352549(信息在这个链
 
 # 第二步
 - 构造data
+1
+```
+{"t":1498895696600,"upm":"16148434344","analysisUserId":"118.123.105.20.300811496813701754","guidU":"89eb2d42-da51-4460-85ab-c27f62ff6deb"
+,"cookies":{"CONSUMERCHOICE":"cn/zh_cn","neo.experiments":"{\"main\":{},\"ocp\":{}}","neo.swimlane":"22"
+},"deviceAtlas":"sdevicePixelRatio:1|sdeviceAspectRatio:16/10|bcookieSupport:1","platform":{"id":"nike
+.com","v":"main"},"source":{"id":"dreamcatcher","v":"3.29.2"},"events":[{"pid":"11382127","qty":"1","skuAndSize"
+:"18294708:42","name":"addToCartEvent","t":1498895696597,"url":"https://store.nike.com/cn/zh_cn/pd/air-zoom-pegasus-34-
+男子跑步鞋/pid-11382127/pgid-11631049","swoosh":false,"location":{"cc":"CN","rc":"HN","tp":"vhigh","tz":"GMT
++8","la":"28.20","lo":"112.97","bw":"5000"},"guidS":"12f9ac20-3154-4996-d72c-75242d07dd32"}]}
 
-  
+
+```
+2
+```
+{"t":1498896324473,"upm":"16148434344","analysisUserId":"118.123.105.20.300811496813701754","guidU":"89eb2d42-da51-4460-85ab-c27f62ff6deb"
+,"cookies":{"CONSUMERCHOICE":"cn/zh_cn","neo.experiments":"{\"main\":{},\"ocp\":{}}","neo.swimlane":"22"
+},"deviceAtlas":"sdevicePixelRatio:1|sdeviceAspectRatio:16/10|bcookieSupport:1","platform":{"id":"nike
+.com","v":"main"},"source":{"id":"dreamcatcher","v":"3.29.2"},"events":[{"pid":"11198752","qty":"1","skuAndSize"
+:"17256559:M","name":"addToCartEvent","t":1498896324470,"url":"https://store.nike.com/cn/zh_cn/pd/dry-knit-
+男子短袖跑步上衣/pid-11198752/pgid-11402153","swoosh":false,"location":{"cc":"CN","rc":"HN","tp":"vhigh","tz"
+:"GMT+8","la":"28.20","lo":"112.97","bw":"5000"},"guidS":"12f9ac20-3154-4996-d72c-75242d07dd32"}]}
+```
+3
+```
+{"t":1498897985229,"upm":"16148434344","analysisUserId":"118.123.105.20.300811496813701754","guidU":"89eb2d42-da51-4460-85ab-c27f62ff6deb"
+,"cookies":{"CONSUMERCHOICE":"cn/zh_cn","neo.experiments":"{\"main\":{},\"ocp\":{}}","neo.swimlane":"22"
+},"deviceAtlas":"sdevicePixelRatio:1|sdeviceAspectRatio:16/10|bcookieSupport:1","platform":{"id":"nike
+.com","v":"main"},"source":{"id":"dreamcatcher","v":"3.29.2"},"events":[{"pid":"11597116","qty":"1","skuAndSize"
+:"19264597:36.5","name":"addToCartEvent","t":1498897985227,"url":"https://store.nike.com/cn/zh_cn/pd
+/loden-女子运动鞋/pid-11597116/pgid-11631130","swoosh":false,"location":{"cc":"CN","rc":"HN","tp":"vhigh"
+,"tz":"GMT+8","la":"28.20","lo":"112.97","bw":"5000"},"guidS":"12f9ac20-3154-4996-d72c-75242d07dd32"
+}]}
+```
+
+
+{"t":1498895698156,"upm":"16148434344","analysisUserId":"118.123.105.20.300811496813701754","guidU":"89eb2d42-da51-4460-85ab-c27f62ff6deb"
+,"cookies":{"CONSUMERCHOICE":"cn/zh_cn","neo.experiments":"{\"main\":{},\"ocp\":{}}","neo.swimlane":"22"
+},"deviceAtlas":"sdevicePixelRatio:1|sdeviceAspectRatio:16/10|bcookieSupport:1","platform":{"id":"nike
+.com","v":"main"},"source":{"id":"dreamcatcher","v":"3.29.2"},"events":[{"name":"addToCartSuccessEvent"
+,"t":1498895698154,"url":"https://store.nike.com/cn/zh_cn/pd/air-zoom-pegasus-34-男子跑步鞋/pid-11382127/pgid-11631049"
+,"swoosh":false,"location":{"cc":"CN","rc":"HN","tp":"vhigh","tz":"GMT+8","la":"28.20","lo":"112.97"
+,"bw":"5000"},"guidS":"12f9ac20-3154-4996-d72c-75242d07dd32"}]}
+
+
+
+{"t":1498895966535,"upm":"16148434344","analysisUserId":"118.123.105.20.300811496813701754","guidU":"89eb2d42-da51-4460-85ab-c27f62ff6deb"
+,"cookies":{"CONSUMERCHOICE":"cn/zh_cn","neo.experiments":"{\"main\":{},\"ocp\":{}}","neo.swimlane":"22"
+},"deviceAtlas":"sdevicePixelRatio:1|sdeviceAspectRatio:16/10|bcookieSupport:1","platform":{"id":"nike
+.com","v":"main"},"source":{"id":"dreamcatcher","v":"3.29.2"},"events":[{"name":"addToCartSuccessEvent"
+,"t":1498895966534,"url":"https://store.nike.com/cn/zh_cn/pd/air-zoom-pegasus-34-男子跑步鞋/pid-11382129/pgid-11631049"
+,"swoosh":false,"location":{"cc":"CN","rc":"HN","tp":"vhigh","tz":"GMT+8","la":"28.20","lo":"112.97"
+,"bw":"5000"},"guidS":"12f9ac20-3154-4996-d72c-75242d07dd32"}]}
+
+
+
+
+# adidas问题
+##　购物车
+- 当购物车没有商品程序不能把商品加入购物车，反之可以加入购物车
+
+## 加入地址问题
+- 当程用户没有收货地址序不能加入个人信息，反之可以加入新的收货信息
+
+## 如何判断注册，登录加入购物车，加入个人信息的状态（分别print出来）
+
+
+
+
+product=350198&release2=yes&qty=1&super_attribute%5B184%5D=493&token=e82e0ce22690bf917995ce5994dfab18&isajax=yes
+200
+
+product=350198&release2=yes&qty=1&super_attribute%5B184%5D=493&token=e4fe46de5f2e4dce8ee96da553987bed&isajax=yes
+200
+
+
+loginUrl="https://www.adidas.com.cn/customer/account/loginPost/"
+    loginHead={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36','Referer': 'https://www.adidas.com.cn/customer/account/login/'}
+    nologinCookie=requests.get(loginUrl,headers=loginHead).cookies
+    head= {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+           'Referer': 'https://www.adidas.com.cn/customer/account/login/',
+           'cookie':str(nologinCookie)
+           }
+    data={"login[username]":registerinfo['username'],"login[password]":"112358yS",'send':''}
+    print "login..." 
+    loginHtml = requests.post(loginUrl,data=data,headers = head)
+    #print loginHtml.text
+    loginCookie=loginHtml.cookies
+
+    # # 查看购物车
+    cartUrl='http://www.adidas.com.cn/checkout/cart/'
+    cartHtml=requests.get(cartUrl,headers=loginHead,cookies=loginCookie)
+    loginCookie=cartHtml.cookies
+    print cartHtml.text
+    # #print loginCookie
+    # #添加购物车
+    # ## 获取验证信息
+    spHtml=requests.get(spurl,headers=loginHead,cookies=loginCookie)
+    loginCookie=spHtml.cookies
+    
+    soup=BeautifulSoup(spHtml.text,'html.parser')
+    script=soup.select('script')[-4]
+    productid=str(script).split(',')[2].split('+')[1]
+    infoUrl='http://www.adidas.com.cn/specific/product/ajaxview/?id='+productid
+    infoHml=requests.get(infoUrl,headers=loginHead,cookies=loginCookie)
+    #loginCookie=infoHml.cookies
+
+    #print infoHml
+    infosoup=BeautifulSoup(infoHml.text,'html.parser')
+    token=infosoup.select('input')[0]['value']
+    #print token
+    isajax=infosoup.select('input')[1]['value']
+    #print isajax
+    release2=infosoup.select('input')[2]['value']
+    #print release2
+    product=infosoup.select('input')[3]['value']
+    #print product
+    ## 添加
+    addurl='http://www.adidas.com.cn/checkout/cart/add/'
+    addHead={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+             'Referer': 'https://www.adidas.com.cn/customer/account/login/',
+             'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'
+             }
+    params = {'token':token,'isajax':isajax,'release2':release2,'product':product,'super_attribute[184]':'493','qty':'1'}        
+    data = urllib.urlencode(params)
+    print data
+    #payload = "token="+token+"&isajax="+isajax+"&release2="+release2+"&product="+product+"&super_attribute%5B184%5D="+"811"+"&qty="+"1"
+    #print payload
+
+    add=requests.post(addurl,data=data,headers=addHead,cookies=loginCookie)
+    print add.status_code
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- linksame.py
+++ linksame.py
@@ -6,8 +6,8 @@ from bs4 import BeautifulSoup
 import pytesseract
 import StringIO
 from PIL import Image
-#import urllib
-# ...
+import urllib
+import time 
 
 def nike(spurl,registerinfo,userinfo):
 	# 注册
@@ -42,20 +42,61 @@ def nike(spurl,registerinfo,userinfo):
           slCheck= c["slCheck"],
           sls= c["sls"],
           NIKE_COMMERCE_COUNTRY='CN')
-	print loginCookie
+	# cart
+	# cartUrl='https://secure-store.nike.com/cn/checkout/html/cart.jsp'
+	# cartHtml=requests.get(cartUrl,headers=head,cookies=loginCookie).text
+	# print cartHtml
+
 	# 商品页面
-	#sphtml = requests.get(spurl,headers = head,cookies=loginCookie)
-	#print sphtml.text
+	# sphtml = requests.get(spurl,headers = head,cookies=loginCookie)
+	# print sphtml.text
 	# 添加购物车
-	# addUrl='https://nod.nikecloud.com/nod/rest/intake'
-	# paydata={"t":1498726926137,"upm":"16138156515","analysisUserId":loginCookie['AnalysisUserId'],"guidU":"9e35ac0b-c90c-4724-e2f0-1bc70651ff05","cookies":{"CONSUMERCHOICE":"cn/zh_cn","neo.experiments":"{\"main\":{},\"snkrs\":{},\"ocp\":{},\"thirdparty\":{}}","neo.swimlane":"24"},"deviceAtlas":"sdevicePixelRatio:1|sdeviceAspectRatio:16/9|bcookieSupport:1","platform":{"id":"nike.com","v":"main"},"source":{"id":"dreamcatcher","v":"3.29.2"},"events":[{"pid":"11819614","qty":"1","skuAndSize":"19844165:40","name":"addToCartEvent","t":1498726926134,"url":"https://store.nike.com/cn/zh_cn/pd/zoom-kd10-ep-男子篮球鞋/pid-11819614/pgid-11852285","swoosh":'false',"location":{"cc":"CN","rc":"HB","tp":"vhigh","tz":"GMT+8","la":"30.58","lo":"114.27","bw":"5000"},"guidS":"8b35890e-c179-4483-fc8f-c6e33962e927"}]}
-	# addcart=requests.post(addUrl,headers=head,data=paydata,cookies=loginCookie)
-	# print addcart.status_code
+	addUrl='https://nod.nikecloud.com/nod/rest/intake'
+	payload={"t":1,
+			 "upm":1,
+			 "analysisUserId":loginCookie['AnalysisUserId'],
+			 "guidU":1,
+			 "cookies":{"CONSUMERCHOICE":"cn/zh_cn",
+			 			"neo.experiments":1,
+			 			"neo.swimlane":1},
+			 "deviceAtlas":1,
+			 "platform":{"id":"nike.com","v":"main"},
+			 "source":{"id":"dreamcatcher","v":"3.29.2"},
+			 "events":[{"pid":1,
+			 			"qty":"1",
+			 			"skuAndSize":1,
+			 			"name":"addToCartEvent",
+			 			"t":1,
+			 			"url":1,
+			 			"swoosh":'false',
+			 			"location":{"cc":"CN","rc":"BJ","tp":"vhigh","tz":"GMT+8","la":"39.90","lo":"116.41","bw":"5000"},
+			 			"guidS":1}]}
+	print payload
+	addcart=requests.post(addUrl,headers=head,data=payload,cookies=loginCookie)
+	print addcart.status_code
+	# payload2={"t":str(int((time.time())*1000)),
+	# 		  "upm":"16154697162",
+	# 		  "analysisUserId":loginCookie['AnalysisUserId'],
+	# 		  "guidU":"89eb2d42-da51-4460-85ab-c27f62ff6deb",
+	# 		  "cookies":{"CONSUMERCHOICE":"cn/zh_cn",
+	# 		  			 "neo.experiments":"{\"main\":{},\"ocp\":{}}",
+	# 		  			 "neo.swimlane":"22"},
+	# 		  "deviceAtlas":"sdevicePixelRatio:1|sdeviceAspectRatio:16/10|bcookieSupport:1",
+	# 		  "platform":{"id":"niken"},
+	# 		  "source":{"id":"dreamcatcher","v":"3.29.2"},
+	# 		  "events":[{"name":"addToCartSuccessEvent",
+	# 		  "t":str(int((time.time())*1000)),
+	# 		  "url":"https://store.nike.com/cn/zh_cn/pd/loden-女子运动鞋/pid-11597119/pgid-11631130",
+	# 		  "swoosh":'false',
+	# 		  "location":{"cc":"CN","rc":"BJ","tp":"vhigh","tz":"GMT+8","la":"39.90","lo":"116.41","bw":"5000"},
+	# 		  "guidS":"4d595ede-90c9-46a7-ebcc-7845741cf176"}]}
+	# addcart2=requests.post(addUrl,headers=head,data=payload2,cookies=loginCookie)
+	# print addcart2.status_code
 	# addUrl2='https://nod.nikecloud.com/nod/rest/intake'
 	# paydata2={"t":1498726927281,"upm":"16138156515","analysisUserId":"WUDmUwoMQ10AACGkllAAAARv","guidU":"9e35ac0b-c90c-4724-e2f0-1bc70651ff05","cookies":{"CONSUMERCHOICE":"cn/zh_cn","neo.experiments":"{\"main\":{},\"snkrs\":{},\"ocp\":{},\"thirdparty\":{}}","neo.swimlane":"24"},"deviceAtlas":"sdevicePixelRatio:1|sdeviceAspectRatio:16/9|bcookieSupport:1","platform":{"id":"nike.com","v":"main"},"source":{"id":"dreamcatcher","v":"3.29.2"},"events":[{"name":"addToCartSuccessEvent","t":1498726927278,"url":"https://store.nike.com/cn/zh_cn/pd/zoom-kd10-ep-男子篮球鞋/pid-11819614/pgid-11852285","swoosh":'false',"location":{"cc":"CN","rc":"HB","tp":"vhigh","tz":"GMT+8","la":"30.58","lo":"114.27","bw":"5000"},"guidS":"8b35890e-c179-4483-fc8f-c6e33962e927"}]}
 	# addcart2=requests.post(addUrl2,headers=head,data=paydata2,cookies=loginCookie)
 	# print addcart2.status_code
-	#return payurl 
+	# return payurl
 	
 def adidas(spurl,registerinfo,userinfo):
 	# 注册
@@ -97,33 +138,34 @@ def adidas(spurl,registerinfo,userinfo):
 	#          'username':registerinfo['username'],
 	#          'password':'112358yS',
 	#          'confirmation':'112358yS',
-	#          'agree_terms':1
-	# }
+	#          'agree_terms':1}
 	# reghtml=requests.post(regUrl,data=regData,headers =regHead)
+	# #titlesoup=BeautifulSoup(reghtml.text,'html.parser')
+	# # print title
+	# # print reghtml.status_code
 	# print reghtml.text
 	# loginCookie=reghtml.cookies
+	# print loginCookie
 	# 登陆
 	loginUrl="https://www.adidas.com.cn/customer/account/loginPost/"
 	loginHead={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36','Referer': 'https://www.adidas.com.cn/customer/account/login/'}
 	loginCookie=requests.get(loginUrl,headers=loginHead).cookies
-	#print loginCookie
+
 	head= {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
 	       'Referer': 'https://www.adidas.com.cn/customer/account/login/',
 	       'cookie':str(loginCookie)
 	       }
 	data={"login[username]":registerinfo['username'],"login[password]":"112358yS",'send':''}
 	loginHtml = requests.post(loginUrl,data=data,headers = head)
-	#print loginHtml.text
+
 	loginCookie=loginHtml.cookies
-	#print loginCookie
-	#print loginCookie
+	
 	# 查看购物车
 	cartUrl='http://www.adidas.com.cn/checkout/cart/'
 	cartHtm=requests.get(cartUrl,headers=loginHead,cookies=loginCookie)
 	print cartHtm.text
-	#print loginCookie
 	#添加购物车
-	## 获取验证信息
+	#1 获取验证信息
 	spHtml=requests.get(spurl,headers=loginHead,cookies=loginCookie)
 	
 	soup=BeautifulSoup(spHtml.text,'html.parser')
@@ -132,17 +174,13 @@ def adidas(spurl,registerinfo,userinfo):
 	infoUrl='http://www.adidas.com.cn/specific/product/ajaxview/?id='+productid
 	infoHml=requests.get(infoUrl,headers=loginHead,cookies=loginCookie)
 
-	#print infoHml
 	infosoup=BeautifulSoup(infoHml.text,'html.parser')
 	token=infosoup.select('input')[0]['value']
-	#print token
 	isajax=infosoup.select('input')[1]['value']
-	#print isajax
 	release2=infosoup.select('input')[2]['value']
-	#print release2
 	product=infosoup.select('input')[3]['value']
-	#print product
-	## 添加
+
+	## 2 添加
 	addurl='http://www.adidas.com.cn/checkout/cart/add/'
 	addHead={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
 			 'Referer': 'https://www.adidas.com.cn/customer/account/login/',
@@ -150,12 +188,59 @@ def adidas(spurl,registerinfo,userinfo):
 	#params = {'token':token,'isajax':isajax,'release2':release2,'product':product,'super_attribute[185]':'811','qty':'1'}        
 	# data = urllib.urlencode(params) 
 	#print data
-	payload = "token="+token+"&isajax="+isajax+"&release2="+release2+"&product="+product+"&super_attribute%5B185%5D="+"811"+"&qty="+"1"
+	payload = "token="+token+"&isajax="+isajax+"&release2="+release2+"&product="+product+"&super_attribute%5B185%5D="+"69"+"&qty="+"1"
 	print payload
 	add=requests.post(addurl,data=payload,headers=addHead,cookies=loginCookie)
 	print add.status_code
+	
+	
+	# add addres
+	# infoUrl='https://www.adidas.com.cn/yancheckout/process/'
+	# head={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+	# 		 'Referer': 'https://www.adidas.com.cn/customer/account/login/'}
 
-
+	# infohtml=requests.get(infoUrl,headers=head,cookies=loginCookie)
+	# soup=BeautifulSoup(infohtml.text,"html.parser")
+	# token=soup.select('input')[20]['value']
+	# loginCookie=infohtml.cookies
+	# print token
+	# addresUrl='https://www.adidas.com.cn/yancheckout/process/saveShippingAndPayment/'
+	# userinfo={"shipping[firstname]":"yangsheng",
+	# 		  "shipping[country_id]":"CN",
+	# 		  "shipping[region_id]":"515",
+	# 		  "shipping[region]":"天津",
+	# 		  "shipping[city_id]":"342",
+	# 		  "shipping[district_id]":"3127",
+	# 		  "shipping[city]":"天津市",
+	# 		  "shipping[district]":"河东区",
+	# 		  "shipping[street][]":"苏家爱华yangsheng21",
+	# 		  "shipping[postcode]":"432000",
+	# 		  "shipping[mobile]":"17786493931",
+	# 		  "shipping[tel_areacode]":"",
+	# 		  "shipping[telephone]":"",
+	# 		  "shipping[save_in_address_book]":"1",
+	# 		  "shipping[use_for_shipping]":"1",
+	# 		  "shipping[update_region]":"0",
+	# 		  "shipping[primary_shipping]":"1",
+	# 		  "shipping[primary_billing]":"1",
+	# 		  "shipping[delivery_memo]":"湖区yangsheng2",
+	# 		  "shipping[id]":"",
+	# 		  "shipping[yancheckout_page]":"1",
+	# 		  "delivery_type":"Normal",
+	# 		  "token":token,
+	# 		  "payment[alipay_pay_bank]":"ALIPAY",
+	# 		  "payment[alipay_pay_method]":"bankPay",
+	# 		  "shipping_method":"carrier_bestway",
+	# 		  "fapiao[fapiao_type]":"personal",
+	# 		  "fapiao[fapiao_title]":"",
+	# 		  "fapiao[fapiao_memo]":""}
+	# data = urllib.urlencode(userinfo)
+	# print data
+	# add_head={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.125 Safari/537.36',
+	# 		 'Referer': 'https://www.adidas.com.cn/yancheckout/process/',
+	# 		 'Content-Type':'application/x-www-form-urlencoded'}
+	# add_addres=requests.post(addresUrl,headers=add_head,data=data,cookies=loginCookie)
+	# print add_addres.status_code
 	#return payurl
 
 def apple(spurl,registerinfo,userinfo):
@@ -170,9 +255,9 @@ appleUrl='https://www.apple.com/cn/'
 
 # 动态数据
 weburl='http://www.adidas.com.cn/'
-spurl='http://www.adidas.com.cn/cf9797'
+spurl='http://www.adidas.com.cn/by4472'
 userinfo={}
-registerinfo={'email':'ysnike123456@qq.com','mobile':'17786495627','username':'ysadidas1998'}
+registerinfo={'email':'yangsheng1994@qq.com','mobile':'17786493837','username':'adidasys1994'}
 
 
 # 平台选择
